131bf7683318017ec09f3203ccee132fe80b4585
ReGa-Lee <sxakln2015@163.com>
2017-04-10 17:06:31 +0800

    最长公共子串的空间优化

--- 四 递归与动态规划/5.最长公共子串&空间优化.cpp
+++ 四 递归与动态规划/5.最长公共子串&空间优化.cpp
@@ -26,7 +26,7 @@ int[][] getdp(char[] str1, char[] str2){
 	//return dp[str1.length-1][str2.length-1];	//	×	想要得到长度直接遍历dp
 	return dp;
 }
-//遍历dp得到最大值及其位置->自然获得公共子串
+//	遍历dp得到最大值及其位置->自然获得公共子串
 char[] lcst1(char[] str1, char[] str2){
 	if(NULL == str1 || NULL == str2 || 0 == str1.length || 0 == str2.length)
 		return -1;
@@ -45,7 +45,40 @@ char[] lcst1(char[] str1, char[] str2){
 	strncpy(rst, str1 + end - max, max);	//	实现从str1的第end-max个字符开始复制max个字符给rst
 	return rst;
 }
-//根据dp[i-1][j-1]进行空间优化	O-(M*N) --> O-(1)
+//	根据dp[i-1][j-1]进行空间优化	O-(M*N) --> O-(1)
 char[] lcst2(char[] str1, char[] str2){
-	
+	if(NULL == str1 || NULL == str2 || 0 == str1.length || 0 == str2.length)
+		return -1;
+	int row = 0;	//	斜线开始位置的行
+	int col = str2.length - 1;	//	斜线开始位置的列
+	int max = 0;	//	最大长度
+	int end = 0;	//	最大长度更新时，记录子串结尾位置
+	while (row < str1.length){
+		int i = row;
+		int j = col;
+		int len = 0;
+		//	从[i][j]向正右下方遍历
+		while (i < str1.length && j < str2.length){
+			if(str1[i] != str2[j])
+				len = 0;
+			else
+				len++;
+			//	记录最大值及结束字符的位置
+			if(len > max){
+				max = len;
+				end = i;
+			}
+			//	斜线方向比较(同时++)
+			i++;
+			j++;
+		}
+		//	起始位置在[col][row] --> [i][0]
+		if (col > 0)
+			col--;	//	斜线开始位置的列先向左移动 即 [i][0] --> [i-1][0] -i-> [0][0]
+		else
+			row++;	//	列不能移动时，行向下移动 即[0][0] --> [0][0+1] -j-> [0][j]
+	}
+	int rst[str1.length];
+	strncpy(rst, str1 + end - max, max);	//	实现从str1的第end-max个字符开始复制max个字符给rst
+	return rst;
 }
