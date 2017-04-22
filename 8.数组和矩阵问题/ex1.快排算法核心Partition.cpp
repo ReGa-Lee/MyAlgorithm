12ff33b1cecba1f7a80862cb182047bde46af89e
ReGa-Lee <sxakln2015@163.com>
2017-04-22 22:18:45 +0800

    Partition拓展思路

--- 8.数组和矩阵问题/ex1.快排算法核心Partition.cpp
+++ 8.数组和矩阵问题/ex1.快排算法核心Partition.cpp
@@ -36,4 +36,77 @@ void QuickSort(int[] arr, int length, int start, int end){
 		Partition(arr, length, index + 1, end);
 }
 
-//实现
+//数组中出现次数超过一半的数字(中位数问题)
+int HalfNUmber(int[] arr, int length){
+	if(CheckInvalidArray(arr, length))	//	NULL输入
+		return -1;
+	
+	int mid = length >> 1;
+	int start = 0;
+	int end = length - 1;
+	int index = Partition(arr, length, start, end);
+	while(index != mid){	//	查找中位数
+		if(index > mid){
+			end = index - 1;
+			Partition(arr, length, start, end);
+		}else{
+			start = index + 1;
+			Partition(arr, length, start, end);
+		}
+	}
+	
+	int rst = arr[mid];
+	if(!CheckHalf(arr, length, rst))
+		return -1;
+	
+	return rst;
+}
+//	NULL输入的情况
+bool bInputInvalid = false;
+
+bool CheckInvalidArray(int[] arr, int length){
+	bInputInvalid = false;
+	if(NULL == arr || length <= 0)
+		bInputInvalid = true;
+	
+	return bInputInvalid;
+}
+//	非NULL输入但出现次数不超过一半的情况
+bool CheckHalf(int[] arr, int length, int rst){
+	int times = 0;
+	for(int i = 0; i < length; i++){
+		if(arr[i] == rst)
+			times++;
+	}
+	
+	bool isMoreThanHalf = true;
+	if(times * 2 <= length){
+		bInputInvalid = true;
+		isMoreThanHalf = false;
+	}
+	
+	return isMoreThanHalf;
+}
+//最小K个数
+void GetLeastNumber(int[] input, int n, int[] output, int k){
+	if(NULL == input || NULL == output || k > n || n <= 0 || k <= 0)
+		return;
+	
+	int start = 0;
+	int end = n - 1;
+	int index = Partition(input, n, start, end);
+	while(index != k - 1){	//	0~k-1个数
+		if(index > k - 1){
+			end = index - 1;
+			Partition(input, n, start, end);
+		}else{
+			start = index + 1;
+			Partition(input, n, start, end);
+		}
+	}
+	
+	for(int i = 0; i < k; i++){
+		output[i] = input[i];
+	}
+}
+//	最小K个数 之 海量数据处理(基于堆或红黑树)
