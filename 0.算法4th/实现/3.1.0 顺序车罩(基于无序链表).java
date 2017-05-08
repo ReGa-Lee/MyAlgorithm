046c14ab05bc254c4c6296874c4055f398b76f97
ReGa-Lee <sxakln2015@163.com>
2017-05-08 23:44:01 +0800

    基于链表的符号表

---
+++ 0.算法4th/实现/3.1.0 顺序车罩(基于无序链表).java
@@ -0,0 +1,37 @@
+﻿/*
+API - MaxPQ
+Value get(Key)
+void put(Key, Value)
+*/
+
+public class SequentialSearchST<Key, Value>{
+	private Class Node{
+		Key key;
+		Value value;
+		Node next;
+		
+		public Node(Key key, Value value, Node next){
+			this.key = key;
+			this.value = value;
+			this.next = next;
+		}
+	}
+	
+	private Node first;	//	链表首结点
+	
+	public Value get(Key key){	//	遍历查找给定的键，返回相关联的值
+		for(Node x = first; x != null; x = x.next)
+			if(key.equals(x.key))
+				return x.value;
+		return null;
+	}
+	
+	public void put(Key key, Value value){	//	遍历查找给定的键，更新其值，否则创建新键值对在链首
+		for(Node x = first; x != null; x = x.next)
+			if(key.equals(x.key)){
+				x.value = value;
+				return;
+			}
+		first = new Node(key, value, next);
+	}
+}
