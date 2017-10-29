b738a495a7bd49a20f1d91016200ef148e128263
ReGa-Lee <sxakln2015@163.com>
2017-10-29 17:25:11 +0800

    update BinTree.cpp

--- 《数据结构》/BinTree.cpp
+++ 《数据结构》/BinTree.cpp
@@ -156,9 +156,89 @@ void travPre_I2 (BinNodePosi(T) x, VST& visit) {
     }
 }
 // 后序遍历 迭代
-
-// 中序遍历 迭代
-
+template <typename T> //在以S栈顶节点为根的子树中，找最高左侧可见叶节点，并使
+static void gotoHLVFL (Stack<BinNodePosi(T)>& S) { //沿途遇见节点依次入栈
+    while (BinNodePosi(T) x = S.top()) { //自顶向下，反复检查当前节点
+        if (HasLChild(*x)) { //尽可能向左(找的是最高左侧...)
+            if (HasRChild(*x)) S.push(x->rc); //有右子 先push
+            S.push(x->lc); 
+        } else //实在不得已
+            S.push(x->rc); //才向右
+    }
+    S.pop(); //返回之前，弹出栈顶的空节点(while在S.top为空时才结束)
+} 
+template <typename T, typename VST> 
+void travPost_I (BinNodePosi(T) x, VST& visit) {
+    Stack<BinNodePosi(T)> S;
+    if (x) S.push(x);
+    while (!S.empty()) {
+        if (S.top() != x->parent) //若栈顶非当前节点之父(必是其右兄)
+            gotoHLVFL(S); //在以其右兄为根的子树中，找到HLVFL(递归深入其中并依次入栈)
+        x = S.pop(); visit ( x->data ); //弹出栈顶(即前一节点之后继?)，并访问之
+    }
+}
+// 中序遍历 迭代1
+template <typename T> //从当前节点出发，沿左分支不断深入，直到没有左分支的节点
+static void goAlongLeftBrach (BinNodePosi(T) x, Stack<BinNodePosi(T)>& S) {
+    while (x) { S.push(x); x = x->lc; } 
+}
+template <typename T, typename VST>
+void travIn_I1 (BinNodePosi(T) x, VST& visit) {
+    Stack<BinNodePosi(T)> S;
+    while (true) {
+        goAlongLeftBrach(x, S); //从当前节点出发，逐批入栈
+        if (S.empty()) break; //直到栈空退出
+        x = S.pop(); visit ( x->data ); //弹出栈顶并访问
+        x = x->rc; //转入其右子
+    }
+}
+// 中序遍历 迭代2 
+template <typename T, typename VST>
+void tarvIn_I2 (BinNodePosi(T) x, VST& visit) {
+    Stack<BinNodePosi(T)> S;
+    while (ture) {
+        if (x) {
+            S.push(x); //当前根节点入栈
+            x = x->lc; //深入遍历左子树
+        } else if (!S.empty()) {
+            x = S.pop(); //尚未访问的最低祖先节点出栈
+            visit ( x->data ); //访问该祖先节点
+            x = x->rc; //遍历祖先的右子
+        } else 
+            break;
+    }
+}
+// 中序遍历 迭代3 无栈的就地算法
+template <typename T> //定位节点v的直接后继
+BinNodePosi(T) BinNode<T>::succ() { 
+    BinNodePosi(T) s = this; //记录后继的临时变量
+    if (rc) { //若有右子，直接后继必在右子树中的最靠左(最小)的节点
+        s = rc;
+        while (HasLChild(*s)) s = s->lc;
+    } else { //否则，直接后继是"将当前节点包含于其左子树中的最低祖先"，即
+        while (IsRChild(*s)) s = s->parent; //逆向地沿父分支，不断朝左上移动
+        s = s->parent; //再往上一步，即直接后继(如果存在)
+    }
+    return s;
+}
+template <typename T, typename VST> //由于v直接后继无非为 v的右子 或 v的某一祖先 可用回溯标记
+void tarvIn_I3 (BinNodePosi(T) x, VST& visit) {
+    bool backtrack = false; //前一步是否刚从右子树回溯(即右子为空的情况)
+    while (true) {
+        if (!backtrack && HasLChild(*x)) //若有左子且不是刚刚回溯
+            x = x->lc; //深入左子
+        else { //否则为无左子或者刚刚回溯(相当于无左子树)
+            visit ( x->data );
+            if (HasRChild(*x)) { //若右子非空
+                x = x->rc; //深入右子
+                backtrack = false; 
+            } else { //右子为空
+                if (!(x = x->succ())) break; //回溯(包含抵达中序意义下最后一个节点时退出返回)
+                backtrack = true; 
+            }
+        }
+    }
+}
 // 层次遍历
 template <typename T, typename VST> 
 void BinNode<T>::travLevel (VST& visit) {
